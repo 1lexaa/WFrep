b8572cc7799bc180232a1ea03fa9bc7b0b0c3a3a
1lexaa <nice140@icloud.com>
2022-09-01 17:07:05 +0300

    dofg ks

--- 1sept/Form1.Designer.cs
+++ 1sept/Form1.Designer.cs
@@ -41,6 +41,7 @@
             this.button1.Text = "button1";
             this.button1.UseVisualStyleBackColor = true;
             this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            this.button1.MouseEnter += new System.EventHandler(this.button1_MouseEnter);
             // 
             // buttonCounter
             // 
--- 1sept/Form1.cs
+++ 1sept/Form1.cs
@@ -1,3 +1,5 @@
+using System.Diagnostics.Metrics;
+
 namespace _1sept
 {
     public partial class Form1 : Form
@@ -9,19 +11,37 @@ namespace _1sept
         }
 
         //private int counter = 0;
-        private int k = 0;
+        //private int k = 0;
 
 
+        private int counter = 0;
+        Random random = new Random();
 
-       
 
         private void button1_Click_1(object sender, EventArgs e)
         {
             // counter++;
-            Button b = sender as Button;
-          
-            button1.Text = $"{k++}";
+            // Button b = sender as Button;
+
+            // button1.Text = $"{k++}";
             // buttonCounter.Text = counter.ToString();
+
+
+            
+
+            counter++;
+
+            buttonCounter.Text = counter.ToString();
+
         }
+
+        private void button1_MouseEnter(object sender, EventArgs e)
+        {
+            button1.Location = new Point(random.Next(0, this.Width - button1.Width), random.Next(0, this.Height - button1.Height));
+        }
+
+        //
+        //
+        
     }
 }
